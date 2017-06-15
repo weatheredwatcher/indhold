074e0a66af2df11a615731a97047c0eb1e60629f
weatheredwatcher <weatheredwatcher@therabbithole.weatheredwatcher.net>
2017-06-15 15:53:45 -0400

    removed some debug code and whiteapce from register.php

--- config/register.php
+++ config/register.php
@@ -34,7 +34,6 @@
 			   use Silex\Provider\FormServiceProvider;
 			   $app->register(new FormServiceProvider());
 
-
 			   $app->register(new Silex\Provider\LocaleServiceProvider());
 			   $app->register(new Silex\Provider\TranslationServiceProvider(), array(
 			       'locale_Fallbacks' => array('en'),
@@ -42,8 +41,3 @@
 			   $app->register(new Silex\Provider\SessionServiceProvider(), array(
     			   'cookie_lifetime' => 216000,
 			   ));
-
-
-
-
-error_log( __DIR__.'/views');
