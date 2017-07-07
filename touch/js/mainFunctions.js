744e688d76b0ca3a19ce0f47b65fc38b87d66f5a
tlowryot <tlowry@opentext.com>
2017-07-07 13:12:07 -0400

    Finishing up leaderboard styles in iFrame

--- touch/js/mainFunctions.js
+++ touch/js/mainFunctions.js
@@ -755,7 +755,7 @@ $(document).ready(function() {
 	
 	
 	//TEAM TWEET FUNCTIONS
-	$('.menuItem-ewgames, .leaderboardButton').click(function() {
+	$('.menuItem-ewgames').click(function() {
 		$('#leaderboardFrame').attr('src', 'http://otew.io/leaderboard/mobile?dev=true');
 		console.log('leader board frame call');
 	});
